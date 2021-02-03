11ee2b0c6fe03e1afd73d60aed36b3130ddc9d8c
Patrick Dang <pdang2@mail.greenriver.edu>
2021-02-03 00:35:10 -0500

    modify 2nd route to take users to form2 page

--- index.php
+++ index.php
@@ -36,7 +36,9 @@ $f3->route('GET /order', function(){
 
 //Define an order2 route
 $f3->route('GET /order2', function(){
-    echo 'order 2 - practice!';
+    //echo 'order 2 - practice!';
+    $view = new Template();
+    echo $view->render('views/form2.html');
 
 });
 
